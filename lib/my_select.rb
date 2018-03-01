894332802cb1702f0d497e4a011bdd52b20323f7
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 00:06:14 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ to_return = []
+ i = 0
+ while i < collection.length
+   temp = yield collection[i]
+   if temp == true
+     to_return << collection[i]
+   end
+ end
+ to_return
 end
