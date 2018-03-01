8e0cad26c6dc42ab6d31a7c46da4aca2e2ea2023
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 00:07:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ to_return = []
+ i = 0
+ while i < collection.length
+   temp = yield collection[i]
+   if temp == true
+     to_return << collection[i]
+   end
+   i+=1
+ end
+ to_return
 end
